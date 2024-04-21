4d88946ceb12099ebd3317d384b211bc60dfbc3a
SainKinteh <sainkinteh@outlook.de>
2024-04-21 19:49:25 +0200

    Initial commit

---
+++ src/test/java/de/OtakuRate/OtakuRateApplicationTests.java
@@ -0,0 +1,13 @@
+package de.OtakuRate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtakuRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
