87ecb4deee9872cbcfb49d3e56bd4f0b42798d30
sain21 <sainkinteh@outlook.de>
2024-05-28 00:44:09 +0200

    Webconfig

--- src/main/java/de/OtakuRate/MyController.java
+++ src/main/java/de/OtakuRate/MyController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.*;
 @RestController
 @CrossOrigin(origins = {"https://otakurate-frontend.onrender.com"})
 public class MyController {
