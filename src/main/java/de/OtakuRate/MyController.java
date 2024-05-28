d038f6be320113cbbd7648a57968d5caf056b89e
sain21 <sainkinteh@outlook.de>
2024-05-28 16:00:15 +0200

    Webconfig

--- src/main/java/de/OtakuRate/MyController.java
+++ src/main/java/de/OtakuRate/MyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 @RestController
-@CrossOrigin(origins = {"https://otakurate-frontend.onrender.com"})
+@CrossOrigin(origins = {"https://otakurate-frontend.onrender.com/rate"})
 public class MyController {
 
     @GetMapping("/rate")
