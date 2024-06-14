51099a176577e8e76e395c9e9e8633cf5cf21bf8
sain21 <sainkinteh@outlook.de>
2024-06-14 13:28:48 +0200

    Meilenstein 4

--- src/main/java/de/OtakuRate/AnimeRatingRepo.java
+++ src/main/java/de/OtakuRate/AnimeRatingRepo.java
@@ -1,6 +1,8 @@
 package de.OtakuRate;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnimeRatingRepo extends CrudRepository<AnimeRating, Long>{
 }
