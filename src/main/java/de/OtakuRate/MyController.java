96774c587764980559ac97d53deb76357429d05c
sain21 <sainkinteh@outlook.de>
2024-06-25 02:55:23 +0200

    API Jikan entfernt

--- src/main/java/de/OtakuRate/MyController.java
+++ src/main/java/de/OtakuRate/MyController.java
@@ -2,10 +2,6 @@ package de.OtakuRate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 @RestController
@@ -25,15 +21,8 @@ public class MyController {
     public List<AnimeRating> findAllAnimeRatings() {
         return (List<AnimeRating>) animeRatingService.findAllAnimeRatings();
     }
-    @GetMapping("/searchAnime")
-    public String searchAnime(@RequestParam String query) {
-        String url = "https://api.jikan.moe/v4/anime?q=" + query;
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(url, String.class);
-    }
+
+
+
+
 }
-
-
-
-
-
