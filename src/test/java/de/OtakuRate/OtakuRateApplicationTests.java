9c2e355b8ad7f962a4ba90bb2dd3fb27fd4c46ea
sain21 <sainkinteh@outlook.de>
2024-06-29 20:52:50 +0200

    for testing

--- src/test/java/de/OtakuRate/OtakuRateApplicationTests.java
+++ src/test/java/de/OtakuRate/OtakuRateApplicationTests.java
@@ -5,4 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OtakuRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    throw new RuntimeException("only for test");
+    }
 }
